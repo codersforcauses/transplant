67f2d5ee3eb98862c1edc9a1642e6c58667b1769
Mel <20352574@student.curtin.edu.au>
2025-06-28 08:46:39 +0000

    Creating User and Participant in admin.py

--- server/event_registration/admin.py
+++ server/event_registration/admin.py
@@ -1,3 +1,12 @@
-# from django.contrib import admin
+#from unfold.admin import ModelAdmin, StackedInline
+from .models import User, Participant
+from django.contrib import admin
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ""
+
+    
+@admin.register(Participant)
+class ParticipantAdmin(ModelAdmin, ImportExportMopdelAdmin):
+    
