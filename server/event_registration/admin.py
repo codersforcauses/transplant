a7886ff98516a573af632a44a66558024e57041c
Mel <20352574@student.curtin.edu.au>
2025-06-28 08:47:11 +0000

    Updates

--- server/event_registration/admin.py
+++ server/event_registration/admin.py
@@ -1,12 +1,13 @@
-#from unfold.admin import ModelAdmin, StackedInline
+# from django.forms import ModelAdmin
 from .models import User, Participant
 from django.contrib import admin
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    fields = ""
+    fields = ("email", "password", "is_admin", "created_at", "updatedx_at", "last_login")
 
     
 @admin.register(Participant)
-class ParticipantAdmin(ModelAdmin, ImportExportMopdelAdmin):
-    
+class ParticipantAdmin(admin.ModelAdmin):
+    fields = ("email", "password", "is_admin", "created_at", "updatedx_at", "last_login")
