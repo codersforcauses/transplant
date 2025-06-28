df91719d3abd4c5e9d049f3ab9d704f45284a942
Mel <20352574@student.curtin.edu.au>
2025-06-28 10:44:33 +0000

    creating UserManager

--- server/event_registration/models.py
+++ server/event_registration/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.utils import timezone
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set.")
+        email = self.normalize_email(email)
+        extra_fields.setdefault("is_active", True)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
 class User(AbstractBaseUser, PermissionsMixin):
     ROLE_CHOICES = (
         ("ADMIN", "Admin"),
@@ -22,6 +34,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
