585c2890ee6a75a33cc599a70b1e98583d81ea15
Mel <20352574@student.curtin.edu.au>
2025-06-28 10:41:53 +0000

    changes to fields in admin.py

--- server/event_registration/admin.py
+++ server/event_registration/admin.py
@@ -1,13 +1,22 @@
-# from django.forms import ModelAdmin
-from .models import User, Participant
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    fields = ("email", "password", "is_admin", "created_at", "updatedx_at", "last_login")
-
-    
-@admin.register(Participant)
-class ParticipantAdmin(admin.ModelAdmin):
-    fields = ("email", "password", "is_admin", "created_at", "updatedx_at", "last_login")
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    list_display = ["email", "role", "is_staff", "is_active", "last_login"]
+    search_fields = ["email"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("role",)}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "created_at", "updated_at")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "role", "is_active", "is_staff", "is_superuser"),
+        }),
+    )
