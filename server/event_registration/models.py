282ccd6397a265cb9e17e509515f4e40b9536621
Mel <20352574@student.curtin.edu.au>
2025-06-28 10:42:25 +0000

    Setting up models.py

--- server/event_registration/models.py
+++ server/event_registration/models.py
@@ -1,3 +1,6 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+
+
