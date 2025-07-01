2ebc8c92a0c73e48637873f503d0b8234cf27481
Nico Buchanan <nico.s.buchanan@gmail.com>
2025-07-01 05:41:00 +0000

    Added spacing (padding/gaps) and font styling to match Figma design (desktop light-mode). Still needs some tweaking to look exactly the same.

--- client/src/components/ui/Auth/Forms/signup.tsx
+++ client/src/components/ui/Auth/Forms/signup.tsx
@@ -14,60 +14,85 @@ const SignUpForm = () => {
   return (
     <div
       className={cn(
-        "py-15 flex min-h-screen flex-col items-center px-5 font-sans",
+        "flex min-h-screen flex-col items-center justify-center px-5 font-sans",
         fontSans.variable,
       )}
     >
       <div className="flex w-full max-w-[312px] flex-col items-center justify-center sm:max-w-[628px]">
-        <h1 className="text-3xl text-primary">Sign Up</h1>
-        <p className="text-foreground">
-          Please enter your details to get started.
-        </p>
-        <Button variant="oauth">
-          <GoogleIcon />
-          Continue with Google
-        </Button>
-        <p>Sign up with your email address</p>
-        <form>
-          <label htmlFor="email" className="text-primary">
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            className="rounded-md border border-border bg-input"
-            required
-          />
-          <br />
-          <label htmlFor="password" className="text-primary">
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            className="rounded-md border border-border bg-input"
-            required
-          />
-          <br />
-          <label htmlFor="confirmPassword" className="text-primary">
-            Confirm Password
-          </label>
-          <input
-            id="confirmPassword"
-            type="password"
-            name="confirmPassword"
-            className="rounded-md border border-border bg-input"
-            required
-          />
-          <br />
-          <Button variant="gradient" type="submit">
+        <div className="flex w-full flex-col gap-[25px] pb-[50px] text-center">
+          <h1 className="heading-lg">Sign Up</h1>
+          <p className="body-sm text-subtle-foreground">
+            Please enter your details to get started.
+          </p>
+        </div>
+
+        <div className="flex w-full flex-col justify-center gap-[35px] pb-[35px] text-center">
+          <Button className="h-12" variant="oauth">
+            <GoogleIcon />
+            Continue with Google
+          </Button>
+          <p className="body-sm text-subtle-foreground">OR</p>
+          <p className="body-sm text-subtle-foreground">
+            Sign up with your email address
+          </p>
+        </div>
+
+        <form className="flex w-full flex-col">
+          <div className="flex flex-col space-y-2.5 pb-[30px]">
+            <label htmlFor="email" className="body-sm-medium text-primary">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter Email"
+              className="body-sm h-12 rounded-md border border-border bg-input px-[15px] py-[12px] placeholder-gray-300"
+              required
+            />
+          </div>
+
+          <div className="flex w-full flex-col gap-[30px] pb-[50px] md:flex-row">
+            <div className="flex flex-1 flex-col gap-2.5">
+              <label htmlFor="password" className="body-sm-medium text-primary">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Enter Password"
+                className="body-sm h-12 rounded-md border border-border bg-input px-[15px] py-[12px] placeholder-gray-300"
+                required
+              />
+            </div>
+            <div className="flex flex-1 flex-col gap-2.5">
+              <label
+                htmlFor="confirmPassword"
+                className="body-sm-medium text-primary"
+              >
+                Confirm Password
+              </label>
+              <input
+                id="confirmPassword"
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                className="body-sm h-12 rounded-md border border-border bg-input px-[15px] py-[12px] placeholder-gray-300"
+                required
+              />
+            </div>
+          </div>
+
+          <Button className="body-md" variant="gradient" type="submit">
             Sign Up
           </Button>
         </form>
-        <p>
-          Already have an account? <a href="/login">Login</a>
+        <p className="caption pt-5">
+          Already have an account?{" "}
+          <a className="text-primary" href="/login">
+            Login
+          </a>
         </p>
       </div>
     </div>
