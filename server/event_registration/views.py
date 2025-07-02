ce45ad8742efa8a666b604efee1986c77d6a34a7
James Lee <jameslee6352@gmail.com>
2025-07-02 09:33:17 +0000

    Basic sketching

--- server/event_registration/views.py
+++ server/event_registration/views.py
@@ -1,3 +1,10 @@
-# from django.shortcuts import render
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+UserModel = get_user_model()
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = UserModel
