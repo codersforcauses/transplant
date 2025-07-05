79ac944755f0734051dbb29eb180d252f55a6e68
James Lee <jameslee6352@gmail.com>
2025-07-05 03:25:52 +0000

    Creating user registration form

---
+++ server/event_registration/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserRegistrationForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["email", "password"]
