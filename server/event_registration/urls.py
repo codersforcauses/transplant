b059f1ff3b0eb6e120a8d3126874f8d9f5a483b0
James Lee <jameslee6352@gmail.com>
2025-07-05 03:57:03 +0000

    Tailing slash

--- server/event_registration/urls.py
+++ server/event_registration/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import register_user
 
 urlpatterns = [
-    path("users/register", register_user, name="register_user")
+    path("users/register/", register_user, name="register_user")
 ]
