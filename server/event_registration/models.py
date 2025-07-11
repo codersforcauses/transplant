98430b0af6b373f3b0e3b6c8e0e7689bc0338b2e
James Lee <jameslee6352@gmail.com>
2025-07-11 19:32:58 +0000

    Removing role field nd correcting created date

--- server/event_registration/models.py
+++ server/event_registration/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.utils import timezone
 
 
 class UserManager(BaseUserManager):
@@ -17,26 +16,16 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("role", "ADMIN")
 
         return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    ROLE_CHOICES = (
-        ("ADMIN", "Admin"),
-        ("PARTICIPANT", "Participant"),
-    )
 
     email = models.EmailField(unique=True)
-    role = models.CharField(
-        max_length=20,
-        choices=ROLE_CHOICES,
-        default="PARTICIPANT",
-    )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)  # Controls admin site access
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = "email"
