ce59491fda42f951aecab65bb69d383dc19fdd5e
James Lee <jameslee6352@gmail.com>
2025-07-12 03:39:48 +0000

    Added superuser field to admin page

--- server/event_registration/admin.py
+++ server/event_registration/admin.py
@@ -6,7 +6,7 @@ from .models import User, Registration, RegistrantDetail
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     ordering = ["email"]
-    list_display = ["email", "first_name", "middle_name", "last_name", "is_staff", "is_active", "last_login"]
+    list_display = ["email", "first_name", "middle_name", "last_name", "is_staff", "is_superuser", "is_active", "last_login"]
     search_fields = ["email", "first_name", "last_name"]
     fieldsets = (
         (None, {"fields": ("email", "password")}),
