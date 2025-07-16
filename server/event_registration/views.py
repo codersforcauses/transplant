d2639a4668251e54bafc000bf4c186c63a03233a
James Lee <jameslee6352@gmail.com>
2025-07-16 10:07:20 +0000

    Fixing email error codes

--- server/event_registration/views.py
+++ server/event_registration/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponse
 
 def register_user(request):
     form = UserRegistrationForm(request.POST)
+    errors = form.errors.as_data()
 
-    if form.errors.keys() == "email":
+    if errors.keys() == set(["email"]) and errors["email"][0].code == "unique":
         return HttpResponse(status=409)
 
     if not form.is_valid():
-        print(form.errors)
         return HttpResponse(status=400)
 
     # Email collisions will cause a 400 error, not a 409
